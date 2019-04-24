39f63616f9aa132b3602f6d0bcb7a3c08482e92a
罗文强 <luo_wenqiang@qq.com>
2019-04-24 18:15:47 +0800

    增加令牌管理功能，能够定时更新令牌

--- commons/src/main/java/org/fkjava/wexin/service/impl/AccessTokenManagerSample.java
+++ commons/src/main/java/org/fkjava/wexin/service/impl/AccessTokenManagerSample.java
@@ -6,11 +6,17 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
 
 import org.fkjava.wexin.domain.ResponseError;
 import org.fkjava.wexin.domain.ResponseMessage;
 import org.fkjava.wexin.domain.ResponseToken;
 import org.fkjava.wexin.service.AccessTokenManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,10 +24,48 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class AccessTokenManagerSample implements AccessTokenManager {
 
+	private static final Logger LOG = LoggerFactory.getLogger(AccessTokenManagerSample.class);
+
 	private ObjectMapper objectMapper = new ObjectMapper();
+
+	@Autowired
+	@Qualifier("redisTemplate")
+	private RedisTemplate<String, ResponseToken> redisTemplate;
 
 	@Override
 	public String getToken(String account) throws RuntimeException {
+		String key = "wx_access_token";
+		ResponseToken token = redisTemplate.boundValueOps(key).get();
+		if (token == null) {
+			LOG.trace("数据库里面没有令牌，需要重新获取，正在获取分布式事务锁...");
+			// 加上分布式的事务锁
+			Boolean locked = redisTemplate.boundValueOps(key + "_lock")//
+					// 如果数据库里面本身已经有对应的KEY，那么不能再增加相同的KEY，会等待指定的时间
+					.setIfAbsent(new ResponseToken(), 1, TimeUnit.MINUTES);
+			LOG.trace("获取分布式事务锁结束，结果：{}", locked);
+			if (locked != null && locked == true) {
+				try {
+					token = redisTemplate.boundValueOps(key).get();
+					if (token == null) {
+						LOG.trace("调用远程接口获取令牌");
+						// 再次获取令牌，还是没有令牌，此时就要调用远程接口
+						token = getRemoteToken(account);
+						// 把获取到的令牌，存储到数据库
+						redisTemplate.boundValueOps(key).set(token, token.getExpiresIn(), TimeUnit.SECONDS);
+					}
+				} finally {
+					// 删除分布式事务锁
+					redisTemplate.delete(key + "_lock");
+				}
+			} else {
+				throw new RuntimeException("没有获得事务锁，无法更新令牌");
+			}
+		}
+		return token.getToken();
+	}
+
+	private ResponseToken getRemoteToken(String account) {
+
 		// 此时完全不管过期的问题，也不管开发者的身份问题，调用此方法，总是获取一个新的令牌。
 
 		String appid = "wx375cd9c53c364fc4";
@@ -47,6 +91,9 @@ public class AccessTokenManagerSample implements AccessTokenManager {
 
 			// 获取响应体
 			String body = response.body();
+
+			LOG.trace("调用远程接口的返回值:\n{}", body);
+
 			if (body.contains("errcode")) {
 				// 错误
 				msg = objectMapper.readValue(body, ResponseError.class);
@@ -59,7 +106,7 @@ public class AccessTokenManagerSample implements AccessTokenManager {
 
 			if (msg.getStatus() == 1) {
 				// 成功，返回令牌
-				return ((ResponseToken) msg).getToken();
+				return (ResponseToken) msg;
 			}
 		} catch (Exception e) {
 			throw new RuntimeException("获取访问令牌出现问题：" + e.getLocalizedMessage(), e);
@@ -69,5 +116,4 @@ public class AccessTokenManagerSample implements AccessTokenManager {
 				+ "错误代码=" + ((ResponseError) msg).getErrorCode() //
 				+ "，错误信息=" + ((ResponseError) msg).getErrorMessage());
 	}
-
 }
